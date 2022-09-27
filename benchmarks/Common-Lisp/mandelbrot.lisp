999385f655ad4ede01163a07f0a8a23b1f62b996
q3cpma <q3cpma@posteo.net>
2022-09-28 00:51:28 +0200

    Add mandelbrot
    Current results:
    IMPL \ BENCH               Sieve  Storage  Permute  List   Queens  Bounce  Bounce-opt1  Bounce-opt2  Bounce-opt3  Mandelbrot
    ccl 1.12.1                 383    1895     4081     2724   61      3384    -            -            -            68894
    ccl 1.12.1 (defstruct)     387    1169     3687     315    29      559     522          355          612          -
    sbcl 2.2.8                 1040   690      1580     570    70      1870    -            -            -            31950
    sbcl 2.2.8 (defstruct)     720    590      1850     180    90      360     220          230          220          -
    ecl interp 21.2.1          9190   26630    47530    15480  580     35870   -            -            -            415940
    ecl 21.2.1 (defstruct)     8920   25310    43490    13470  530     21400   22650        20290        21420        -
    ecl compile 21.2.1         1070   15220    17960    6260   240     13460   -            -            -            120750
    ecl 21.2.1 (defstruct)     1090   13200    15320    3740   180     5980    5870         17060        16780        -
    clisp compile 2.49.92      4532   16705    20099    15834  592     10578   -            -            -            858743
    clisp 2.49.92 (defstruct)  4525   13444    15859    4273   302     9041    8773         5836         12897        -
    lua 5.4.4                  1012   6871     3355     2102   2382    2472    -            -            -            21221
    luajit 2.1.0-beta3         144    2317     208      818    259     210     -            -            -            3106
    python3 3.10.6             3427   8644     12094    3855   5717    6408    -            -            -            108771
    node 18.6.0                131    400      201      147    186     149     -            -            -            3003

---
+++ benchmarks/Common-Lisp/mandelbrot.lisp
@@ -0,0 +1,74 @@
+(eval-when (:compile-toplevel :load-toplevel :execute)
+  (load "benchmark"))
+
+(defpackage #:mandelbrot
+  (:use #:cl #:benchmark)
+  (:export #:benchmark #:verify-result))
+(in-package #:mandelbrot)
+
+
+(deftype positive-fixnum ()
+  `(integer 0 ,most-positive-fixnum))
+
+(defun mandelbrot (size)
+  (loop with sum = 0
+		and byte-acc = 0
+		and bit-num = 0
+		for y below size
+		for ci = (- (/ (* 2.0d0 y) size) 1.0d0)
+		do (loop for x below size
+				 for zr = 0.0d0
+				 for zrzr = 0.0d0
+				 for zi = 0.0d0
+				 for zizi = 0.0d0
+				 for cr = (- (/ (* 2.0d0 x) size) 1.5d0)
+				 for z = 0
+				 for not-done = t
+				 for escape = 0
+				 do (loop while (and not-done (< z 50))
+						  do (setf zr (+ (- zrzr zizi) cr)
+								   zi (+ (* 2.0d0 zr zi) ci)
+								   zrzr (* zr zr)
+								   zizi (* zi zi))
+						  if (> (+ zrzr zizi) 4.0d0)
+							do (setf not-done nil
+									 escape 1)
+						  do (incf z))
+				 do (setf byte-acc (+ (ash byte-acc 1) escape)
+						  bit-num (+ bit-num 1))
+				 if (= bit-num 8)
+				   do (setf sum (logxor sum byte-acc)
+							byte-acc 0
+							bit-num 0)
+				 else if (= x (- size 1))
+					do (setf byte-acc (ash byte-acc (- 8 bit-num))
+							 sum (logxor sum byte-acc)
+							 byte-acc 0
+							 bit-num 0))
+		finally (return sum)))
+
+
+(defclass mandelbrot (benchmark) ())
+
+(defmethod inner-benchmark-loop ((self mandelbrot) inner-iterations)
+  (verify-result-override (mandelbrot inner-iterations) inner-iterations))
+
+(defmethod benchmark ((self mandelbrot))
+  (error "Should never be reached"))
+
+(defmethod verify-result ((self mandelbrot) result)
+  (declare (ignore result))
+  (error "Should never be reached"))
+
+(defun verify-result-override (result inner-iterations)
+  (cond
+	((= inner-iterations 500) (= result 191))
+	((= inner-iterations 750) (= result 50))
+	((= inner-iterations 1) (= result 128))
+	(t (progn
+		 (format t "No verification result for ~D found~%" inner-iterations)
+		 (format t "Result is: ~D~%" result)
+		 nil))))
+
+
+;; (format t "~A~%" (mandelbrot 750))
